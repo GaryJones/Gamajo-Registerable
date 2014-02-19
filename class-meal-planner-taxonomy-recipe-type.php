437552f2808f57d0708cc0aaaa959d51c9fdef05
Gary Jones <GaryJones@users.noreply.github.com>
2014-02-19 11:30:21 +0000

    Create class-meal-planner-taxonomy-recipe-type.php
    Example of specific taxonomy object. Note, that at this point, it doesn't get attached to any post type. All we're doing is telling WP about it; relationships between it and one or more post types can happen elsewhere.

---
+++ class-meal-planner-taxonomy-recipe-type.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * Meal Planner
+ *
+ * @package   Meal_Planner
+ * @author    Gary Jones
+ * @link      http://gamajo.com/meal-planner
+ * @copyright 2013 Gary Jones
+ * @license   GPL-2.0+
+ */
+
+/**
+ * Recipe type taxonomy.
+ *
+ * @package Meal_Planner
+ * @author  Gary Jones
+ */
+class Meal_Planner_Taxonomy_Recipe_Type extends Gamajo_Taxonomy {
+	protected $taxonomy = 'mp_recipe_type';
+
+	protected function default_args() {
+		$labels = array(
+			'name'                       => __( 'Recipe Types', 'meal-planner' ),
+			'singular_name'              => __( 'Recipe Type', 'meal-planner' ),
+			'menu_name'                  => __( 'Recipe Types', 'meal-planner' ),
+			'edit_item'                  => __( 'Edit Recipe Type', 'meal-planner' ),
+			'update_item'                => __( 'Update Recipe Type', 'meal-planner' ),
+			'add_new_item'               => __( 'Add New Recipe Type', 'meal-planner' ),
+			'new_item_name'              => __( 'New Recipe Type Name', 'meal-planner' ),
+			'parent_item'                => __( 'Parent Recipe Type', 'meal-planner' ),
+			'parent_item_colon'          => __( 'Parent Recipe Type:', 'meal-planner' ),
+			'all_items'                  => __( 'All Recipe Types', 'meal-planner' ),
+			'search_items'               => __( 'Search Recipe Types', 'meal-planner' ),
+			'popular_items'              => __( 'Popular Recipe Types', 'meal-planner' ),
+			'separate_items_with_commas' => __( 'Separate recipe types with commas', 'meal-planner' ),
+			'add_or_remove_items'        => __( 'Add or remove recipe types', 'meal-planner' ),
+			'choose_from_most_used'      => __( 'Choose from the most used recipe types', 'meal-planner' ),
+			'not_found'                  => __( 'No recipe types found.', 'meal-planner' ),
+		);
+
+		$args = array(
+			'labels'            => $labels,
+			'public'            => true,
+			'show_tagcloud'     => true,
+			'hierarchical'      => false,
+			'rewrite'           => array( 'slug' => 'recipe_type' ),
+			'show_admin_column' => true,
+			'query_var'         => true,
+		);
+
+		return $args;
+	}
+}
